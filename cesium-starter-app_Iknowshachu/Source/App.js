9dc8b6fdaa2e04b99126f808e18a3a3fee163742
aacyagishita <dai-yagishita@aeroasahi.co.jp>
2015-07-09 17:08:50 +0900

    Update App.js

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,4 +1,4 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
 
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://aacyagishita.github.io/2015070
-8_test/cesium-starter-app-Ikowshachu/Source/14kaduno10.kml'));
+8_test/14kaduno10.kml'));
