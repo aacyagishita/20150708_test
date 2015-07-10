0aacdcc1da3b2a5d7b66e161d15d64f638a7c5cb
aacyagishita <dai-yagishita@aeroasahi.co.jp>
2015-07-10 16:45:44 +0900

    Update and rename cesium-starter-app_Iknowshachu/Source/App.js to App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://aacyagishita.github.io/2015070
-8_test/cesium-starter-app-Ikowshachu/Source/aac_77.kml'));
+8_test/cesium-starter-app-Ikowshachu/Source/aac_8.kml'));
