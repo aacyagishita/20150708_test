5ae5a215279026372841721639172870cf7223d5
aacyagishita <dai-yagishita@aeroasahi.co.jp>
2015-07-28 11:19:04 +0900

    20150728

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -2,4 +2,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.extend(Cesium.viewerDragDropMixin);
 //viewer.dataSources.add(Cesium.KmlDataSource.load("aac_8.kml"));
-viewer.dataSources.add(Cesium.KmlDataSource.load("http://aacyagishita.github.io/20150708_test/cesium-starter-app-Iknowshachu/Source/aac_8.kml"));
+viewer.dataSources.add(Cesium.KmlDataSource.load("http://aacyagishita.github.io/20150708_test/cesium-starter-app_Iknowshachu/Source/aac_8.kml"));
