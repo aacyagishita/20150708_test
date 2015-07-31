424d95c28a1f89589fea2bb8f8fce03c65274382
aacyagishita <dai-yagishita@aeroasahi.co.jp>
2015-07-31 11:55:36 +0900

    0731

--- cesium-starter-app_Iknowshachu/Source/Appmob.js
+++ cesium-starter-app_Iknowshachu/Source/Appmob.js
@@ -1,7 +1,7 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 var camera_start = Cesium.Cartesian3.fromDegrees(139.75, 35.66, 1000);
-var camera_direction = Cesium.Cartesian3.fromDegrees(-75.0, 70.0, 0);
+var camera_direction = Cesium.Cartesian3.fromDegrees(0, 0, 0);
 
 viewer.extend(Cesium.viewerDragDropMixin);
 
