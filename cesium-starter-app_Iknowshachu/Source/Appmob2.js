98a48ee1f018fc88c3c48a72e96fb2b95820b192
aacyagishita <dai-yagishita@aeroasahi.co.jp>
2015-08-04 11:28:27 +0900

    0804

--- cesium-starter-app_Iknowshachu/Source/Appmob2.js
+++ cesium-starter-app_Iknowshachu/Source/Appmob2.js
@@ -8,6 +8,10 @@ var camera_direction = Cesium.Cartesian3.fromDegrees(0, 0, 0);
 
 viewer.extend(Cesium.viewerDragDropMixin);
 
+viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(-117.16, 32.71, 15000.0)
+});
+
 //viewer.camera.position(camera_start); 
 //viewer.camera.lookAt(camera_start, camera_direction, Cesium.Cartesian3.UNIT_Z); 
 viewer.dataSources.add(Cesium.CzmlDataSource.load('https://aacyagishita.github.io/20150708_test/Test8.czml'));
