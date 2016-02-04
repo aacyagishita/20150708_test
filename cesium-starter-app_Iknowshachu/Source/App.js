03f4327c1916ff40dd1e83615666e0d278d69c64
root <root@web.gspf.jp>
2016-02-04 17:50:42 +0900

    20160204

--- cesium-starter-app_Iknowshachu/Source/App.js
+++
@@ -1,5 +0,0 @@
-var viewer = new Cesium.Viewer('cesiumContainer');
-
-viewer.extend(Cesium.viewerDragDropMixin);
-//viewer.dataSources.add(Cesium.KmlDataSource.load("aac_8.kml"));
-viewer.dataSources.add(Cesium.KmlDataSource.load("http://aacyagishita.github.io/20150708_test/cesium-starter-app_Iknowshachu/Source/test7.kml"));
